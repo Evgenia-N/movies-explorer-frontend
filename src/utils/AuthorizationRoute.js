596b7cc7635562fd795d9863d00a5a81b0052d40
Evgenia-N <jazzpenguin@yandex.ru>
2022-04-25 00:34:27 +0300

    feat: AuthorizationRoute

---
+++ src/utils/AuthorizationRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
+
+export const AuthorizationRoute = ({ component: Component, loggedIn, ...props }) => {
+  return (
+    <Route>
+      {() =>
+        loggedIn 
+        ? <Redirect to='/movies' />
+        : <Component {...props} /> 
+      }
+    </Route>
+  )
+}
